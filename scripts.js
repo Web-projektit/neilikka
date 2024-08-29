b3ed36ffc873b9bd86b5f67566adf15b50f398cd
Web-projektit <jukka.aula@omnia.fi>
2024-08-29 12:33:18 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    var x = document.querySelector("nav")
+    x.classList.toggle("responsive")
+    }
+/*
+    x.className = x.className ? "responsive" : ""  
+    if (x.className === "") {
+      x.className = "responsive"
+      } 
+    else {
+      x.className = ""
+      }
+    }
+*/
