4c3b9b9cdbfa635a444e79b243e357c04c584323
Web-projektit <jukka.aula@omnia.fi>
2024-09-05 12:54:36 +0300

    bootstrap-lomake

--- scripts.js
+++ scripts.js
@@ -1,14 +1,33 @@
-function myFunction() {
+const myFunction = () => {
     var x = document.querySelector("nav")
     x.classList.toggle("responsive")
     }
-/*
-    x.className = x.className ? "responsive" : ""  
-    if (x.className === "") {
-      x.className = "responsive"
-      } 
-    else {
-      x.className = ""
+
+  var currentPath = location.pathname.split("/").pop();
+  var navLinks = document.querySelectorAll("nav a");
+
+  navLinks.forEach(link => {
+      if (link.getAttribute("href") === currentPath) {
+          link.classList.add("active");
       }
-    }
-*/
+  });
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+  console.log('form validation start')
+   const forms = document.querySelectorAll('.needs-validation')
+  // Loop over them and prevent submission
+  forms.forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+        }
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+
+
+
